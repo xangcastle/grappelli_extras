c5d295d298fde37f5d8a60980c3fc07764f471a5
xangcastle <xangcastle@gmail.com>
2018-11-23 18:54:53 -0600

    __version__ = '1.1.6'

--- testapp/testapp/settings.py
+++ testapp/testapp/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'grappelli_extras.context_processors.extra_menus',
+                'grappelli_extras.context_processors.applist',
             ],
         },
     },
