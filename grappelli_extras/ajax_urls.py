0a6c88c3c21905c9b670ab1ad9863b1f6ed2a666
César Abel Ramírez <xangcastle@gmail.com>
2022-04-21 10:05:32 -0600

    __version__ = '1.2.5'

--- grappelli_extras/ajax_urls.py
+++ grappelli_extras/ajax_urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
+from django.urls import path
 from .ajax import *
 
-# app_label = "grappelli_extras" fixme check before use app label
+app_name = "grappelli_extras"
 
 urlpatterns = [
-    url(r'^get_object/', get_object, name="ajax_getObject"),
-    url(r'^object_update/', object_update, name="ajax_ObjectUpdate"),
-    url(r'^object_view/', object_view, name="ajax_ObjectView"),
-    url(r'^get_collection/', get_collection, name="ajax_getCollection"),
-    url(r'^get_datatables/', get_datatables, name="ajax_getDataTables"),
-    url(r'^autocomplete/', autocomplete, name="ajax_autocomplete"),
-    url(r'^object_execute/', object_execute, name="ajax_objectExecute"),
-    url(r'^get_html_form/', get_html_form, name="ajax_getHtmlForm"),
+    path('get_object/', get_object, name="ajax_getObject"),
+    path('object_update/', object_update, name="ajax_ObjectUpdate"),
+    path('object_view/', object_view, name="ajax_ObjectView"),
+    path('get_collection/', get_collection, name="ajax_getCollection"),
+    path('get_datatables/', get_datatables, name="ajax_getDataTables"),
+    path('autocomplete/', autocomplete, name="ajax_autocomplete"),
+    path('object_execute/', object_execute, name="ajax_objectExecute"),
+    path('get_html_form/', get_html_form, name="ajax_getHtmlForm"),
 ]
